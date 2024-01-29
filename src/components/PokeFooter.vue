4140fbf5bd19d9b6853d7f71b89102232b757e66
Tony CSERESZNYAK <94803453+TonyCse@users.noreply.github.com>
2024-01-29 14:05:58 +0100

    Update PokeFooter.vue

--- src/components/PokeFooter.vue
+++ src/components/PokeFooter.vue
@@ -21,7 +21,12 @@
       justify-content: center;
 }
 .box {
-
+      font-size: 14px;
+      border-radius: 2px;
+      height: 100px;
+      width: 40%;
+      line-height: 16px;
+      color: black;
       background: rgb(221, 221, 221);
       border: 1px solid white;
       box-shadow: 0 1px 0 1px black, inset 0 1px 0 1px black, 0 0 0 1px black, inset 0 0 0 1px black;
